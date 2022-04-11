919e48d733aa5cc314e19730a68a3035344b59c7
Laki Balázs <twarex01@gmail.com>
2022-04-11 23:34:32 +0200

    Skeleton

---
+++ Assets/Scripts/TileGenerator/Biome.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biome
+{
+    public Color color;
+
+    public string name;
+}
---
+++ Assets/Scripts/TileGenerator/Zone.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zone
+{
+    public Color color;
+
+    public string name;
+}
